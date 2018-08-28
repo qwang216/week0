75e608031475e2d8304802628afc54ae74de373c
Jason wang <qwang216@gmail.com>
2018-08-28 17:41:59 -0400

    Initial Commit

---
+++ Week0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week0
+//
+//  Created by Jason wang on 8/27/18.
+//  Copyright © 2018 Jason wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Week0Tests/Week0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Week0Tests.swift
+//  Week0Tests
+//
+//  Created by Jason wang on 8/27/18.
+//  Copyright © 2018 Jason wang. All rights reserved.
+//
+
+import XCTest
+@testable import Week0
+
+class Week0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
